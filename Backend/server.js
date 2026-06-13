57e15af4bc0497038805588c23ef67381cf864b0
Arind1807 <arvindstudy1907@gmail.com>
2026-06-13 12:59:36 +0530

    Prepared backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -24,6 +24,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/workers", workerRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
