b023b56f3e0de6f934435a7cd5fa700f8c8ec736
Arind1807 <arvindstudy1907@gmail.com>
2026-06-15 11:29:27 +0530

    merged login and signup page

--- Fontend/login.js
+++ Fontend/login.js
@@ -59,3 +59,65 @@ function login() {
     });
 
 }
+
+function showRegister() {
+
+    document.getElementById("loginForm").style.display = "none";
+
+    document.getElementById("registerForm").style.display = "block";
+
+}
+
+function showLogin() {
+
+    document.getElementById("registerForm").style.display = "none";
+
+    document.getElementById("loginForm").style.display = "block";
+
+}
+
+function register() {
+
+    const name =
+    document.getElementById("regName").value;
+
+    const email =
+    document.getElementById("regEmail").value;
+
+    const password =
+    document.getElementById("regPassword").value;
+
+    fetch(`${API_URL}/api/users/register`, {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            name,
+            email,
+            password
+        })
+
+    })
+    .then(response => response.text())
+    .then(data => {
+
+        document.getElementById("registerMessage").innerText =
+        data;
+
+        if (data === "User Registered Successfully") {
+
+            setTimeout(() => {
+
+                showLogin();
+
+            }, 1500);
+
+        }
+
+    });
+
+}
